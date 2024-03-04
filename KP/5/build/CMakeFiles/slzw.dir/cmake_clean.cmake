98d8db594adc65d3f44a13d8e4e25c54795c9496
Pavloffff <pavlov.id.2003@gmail.com>
2024-03-04 21:35:47 +0300

    lab7

---
+++ KP/5/build/CMakeFiles/slzw.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/slzw.dir/src/Arifm/Arifm.cpp.o"
+  "CMakeFiles/slzw.dir/src/Arifm/Arifm.cpp.o.d"
+  "CMakeFiles/slzw.dir/src/Arifm/Frequency.cpp.o"
+  "CMakeFiles/slzw.dir/src/Arifm/Frequency.cpp.o.d"
+  "CMakeFiles/slzw.dir/src/Dialog/ArgParser.cpp.o"
+  "CMakeFiles/slzw.dir/src/Dialog/ArgParser.cpp.o.d"
+  "CMakeFiles/slzw.dir/src/Dialog/Dialog.cpp.o"
+  "CMakeFiles/slzw.dir/src/Dialog/Dialog.cpp.o.d"
+  "CMakeFiles/slzw.dir/src/Dialog/Exception.cpp.o"
+  "CMakeFiles/slzw.dir/src/Dialog/Exception.cpp.o.d"
+  "CMakeFiles/slzw.dir/src/Lzw/Lzw.cpp.o"
+  "CMakeFiles/slzw.dir/src/Lzw/Lzw.cpp.o.d"
+  "CMakeFiles/slzw.dir/src/Utils/BitStream.cpp.o"
+  "CMakeFiles/slzw.dir/src/Utils/BitStream.cpp.o.d"
+  "CMakeFiles/slzw.dir/src/Utils/Utils.cpp.o"
+  "CMakeFiles/slzw.dir/src/Utils/Utils.cpp.o.d"
+  "CMakeFiles/slzw.dir/src/main.cpp.o"
+  "CMakeFiles/slzw.dir/src/main.cpp.o.d"
+  "bin/slzw"
+  "bin/slzw.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/slzw.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
